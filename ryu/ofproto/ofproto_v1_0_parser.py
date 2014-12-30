2b5670a3ed69b554252cf68d48f4c513bf0d1403
alyosha1879 <tancok@gmail.com>
2014-12-30 11:51:14 +0900

    Update ofproto_v1_0_parser.py
    クラスの目的を追加した。

--- ryu/ofproto/ofproto_v1_0_parser.py
+++ ryu/ofproto/ofproto_v1_0_parser.py
@@ -14,6 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# あくまでもdecoder/encoder。
+# echoリクエスト等に対してどのような処理をするか、に関する記載はない。
+
 """
 Decoder/Encoder implementations of OpenFlow 1.0.
 """
