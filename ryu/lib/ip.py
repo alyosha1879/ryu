794213cb19f7de3d7c376d8ac8f39e6faa0abb6f
alyosha1879 <tancok@gmail.com>
2015-01-06 16:59:39 +0900

    Update ip.py
    実行例をexampleに追加した。

--- ryu/lib/ip.py
+++ ryu/lib/ip.py
@@ -5,6 +5,9 @@ def ipv4_to_bin(ip):
     '''
         Parse an IP address and return an unsigned int.
         The IP address is in dotted decimal notation.
+        example:
+        >>> ipv4_to_bin("192.168.1.1")
+        '\xc0\xa8\x01\x01'
     '''
     return addrconv.ipv4.text_to_bin(ip)
 
@@ -12,7 +15,11 @@ def ipv4_to_bin(ip):
 def ipv4_to_str(ip):
     """Generate IP address string from an unsigned int.
        ip: unsigned int of form w << 24 | x << 16 | y << 8 | z
-       returns: ip address string w.x.y.z"""
+       returns: ip address string w.x.y.z
+       example:
+       >>> ipv4_to_str('\xc0\xa8\x01\x01')
+       '192.168.1.1'
+    """
     return addrconv.ipv4.bin_to_text(ip)
 
 
