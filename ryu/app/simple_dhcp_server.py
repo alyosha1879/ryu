b56fe18765a72410f84362b8cc29f59df0957259
alyosha1879 <alexei.fyodorovich.karamazov@gmail.com>
2015-09-07 23:33:26 +0900

    add packet_in.

--- ryu/app/simple_dhcp_server.py
+++ ryu/app/simple_dhcp_server.py
@@ -3,6 +3,18 @@
 """
 
 class DHCPServer():
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+
+        pkt = packet.Packet(msg.data)
+
+        udpPacket = pkt.get_protocol(udp.udp)
+ 
+        if udpPacket:
+           pass
 
     def handle_dhcp_request(self):
 
