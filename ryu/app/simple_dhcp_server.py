7458c10e62a37e075cf00cb1c41ab8d8f071c611
alyosha1879 <tancok@gmail.com>
2015-09-09 23:07:20 +0900

    Update simple_dhcp_server.py
    [試験中]
    DHCPパケットの取得に失敗する。
    原因調査中

--- ryu/app/simple_dhcp_server.py
+++ ryu/app/simple_dhcp_server.py
@@ -4,9 +4,14 @@
 import logging
 
 from ryu.base import app_manager
-from ryu.lib.packet import dchp
+from ryu.lib.packet import dhcp
+from ryu.lib.packet import ipv4
+from ryu.controller.handler import set_ev_cls
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER
+from ryu.lib.packet import packet
 
-class DHCPServer():
+class DHCPServer(app_manager.RyuApp):
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         msg = ev.msg
@@ -14,14 +19,18 @@ class DHCPServer():
         ofproto = datapath.ofproto
 
         pkt = packet.Packet(msg.data)
+        #self.logger.info("pkt...%s", pkt)
         dhcpPacket = pkt.get_protocol(dhcp.dhcp)
-        self.logger.info("dchp...%s", dhcpPacket)
- 
+        self.logger.info("dhcp...%s", dhcpPacket)
+
         if dhcpPacket:
            pass
 
     def handle_dhcp_request(self):
+        pass
 
     def handle_dhcp_offer(self):
+        pass
 
-    def handle_dhcp_ack(self):  
+    def handle_dhcp_ack(self):
+        pass
