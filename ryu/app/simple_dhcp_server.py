f23d42699fec4b2f5997e70d0b1384f1a8ee934f
alyosha1879 <tancok@gmail.com>
2015-09-12 10:48:48 +0900

    Update simple_dhcp_server.py
    Discoverメッセージの取得に成功した。

--- ryu/app/simple_dhcp_server.py
+++ ryu/app/simple_dhcp_server.py
@@ -3,8 +3,11 @@
 """
 import logging
 
+import binascii
+
+from ryu.ofproto import ofproto_v1_3
 from ryu.base import app_manager
-from ryu.lib.packet import dhcp
+from ryu.lib.packet import dhcp, udp
 from ryu.lib.packet import ipv4
 from ryu.controller.handler import set_ev_cls
 from ryu.controller import ofp_event
@@ -12,6 +15,8 @@ from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.lib.packet import packet
 
 class DHCPServer(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+  
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         msg = ev.msg
@@ -19,18 +24,19 @@ class DHCPServer(app_manager.RyuApp):
         ofproto = datapath.ofproto
 
         pkt = packet.Packet(msg.data)
-        #self.logger.info("pkt...%s", pkt)
-        dhcpPacket = pkt.get_protocol(dhcp.dhcp)
-        self.logger.info("dhcp...%s", dhcpPacket)
+        udpPacket = pkt.get_protocol(udp.udp)
 
-        if dhcpPacket:
-           pass
-
-    def handle_dhcp_request(self):
+        # check if DHCP Pacet
+        if udpPacket:
+            dhcpPacket = dhcp.dhcp.parser(pkt.protocols[-1])[0]
+            self.logger.info("pkt...%s", dhcpPacket)
+            
+            if dhcpPacket.op == dhcp.DHCP_DISCOVER:
+                self.logger.info("Discover message.") 
+                self.handle_dhcp_request(dhcpPacket) 
+           
+    def handle_dhcp_discover(self, dhcpPacket):
         pass
 
     def handle_dhcp_offer(self):
-        pass
-
-    def handle_dhcp_ack(self):
         pass
