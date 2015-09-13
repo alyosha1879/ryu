642262fc87b3f4218899f89b0274d90efeee6a53
alyosha1879 <alexei.fyodorovich.karamazov@gmail.com>
2015-09-13 18:41:25 +0900

    add _send_packet()

--- ryu/app/simple_dhcp_server.py
+++ ryu/app/simple_dhcp_server.py
@@ -3,12 +3,9 @@
 """
 import logging
 
-import binascii
-
 from ryu.ofproto import ofproto_v1_3
 from ryu.base import app_manager
 from ryu.lib.packet import dhcp, udp
-from ryu.lib.packet import ipv4
 from ryu.controller.handler import set_ev_cls
 from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER
@@ -33,10 +30,30 @@ class DHCPServer(app_manager.RyuApp):
             
             if dhcpPacket.op == dhcp.DHCP_DISCOVER:
                 self.logger.info("Discover message.") 
-                self.handle_dhcp_request(dhcpPacket) 
+                self.handle_dhcp_discover(dhcpPacket, msg, datapath) 
            
-    def handle_dhcp_discover(self, dhcpPacket):
-        pass
+    def handle_dhcp_discover(self, dhcpPacket, msg, datapath):
+ 
+        #msg.buffer = dhcpPacket
+        datapath.send_msg(msg)
+
 
     def handle_dhcp_offer(self):
         pass
+
+    def handle_dhcp_ack(self):
+        pass
+
+    def _send_packet(self, datapath, port, pkt):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        pkt.serialize()
+        self.logger.info("packet-out %s" % (pkt,))
+        data = pkt.data
+        actions = [parser.OFPActionOutput(port=port)]
+        out = parser.OFPPacketOut(datapath=datapath,
+                                  buffer_id=ofproto.OFP_NO_BUFFER,
+                                  in_port=ofproto.OFPP_CONTROLLER,
+                                  actions=actions,
+                                  data=data)
+        datapath.send_msg(out)
