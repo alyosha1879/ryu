2583cd7cbf8b9255321d4b0f6a8d886a240344f4
alyosha1879 <alexei.fyodorovich.karamazov@gmail.com>
2015-09-17 00:03:32 +0900

    commited.

--- ryu/app/simple_dhcp_server.py
+++ ryu/app/simple_dhcp_server.py
@@ -13,7 +13,12 @@ from ryu.lib.packet import packet
 
 class DHCPServer(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
-  
+ 
+    def def __init__(self, *args, **kwargs):
+        self.hw_addr = "08:00:27:b8:0f:8d"
+        self.ip_addr = "192.168.1.1" 
+
+
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         msg = ev.msg
@@ -30,12 +35,21 @@ class DHCPServer(app_manager.RyuApp):
             
             if dhcpPacket.op == dhcp.DHCP_DISCOVER:
                 self.logger.info("Discover message.") 
-                self.handle_dhcp_discover(dhcpPacket, msg, datapath) 
+                self.handle_dhcp_discover(dhcpPacket, datapath) 
+
+            if dhcpPacket.op == dhcp.DHCP_REQEST:
+                self.logger.info("Request message.")
+                #self.handle_dhcp_discover(dhcpPacket, msg, datapath) 
+
            
-    def handle_dhcp_discover(self, dhcpPacket, msg, datapath):
+    def handle_dhcp_discover(self, dhcpPacket, datapath):
  
-        #msg.buffer = dhcpPacket
-        datapath.send_msg(msg)
+        option = dhcp.option(tag=53 ,value='\x02')
+        options = dhcp.options(option_list = [option])
+ 
+        dhcp_pkt = dhcp.dhcp(op=2, chaddr=dhcp_pkt.chaddr, yiaddr=dhcp_pkt.yiaddr, giaddr=dhcp_pkt.giaddr, xid=dhcp_pkt.xid, hlen=6, options=options))
+
+        self._send_dhcp_packet(datapath, dhcp_pkt)
 
 
     def handle_dhcp_offer(self):
@@ -44,7 +58,18 @@ class DHCPServer(app_manager.RyuApp):
     def handle_dhcp_ack(self):
         pass
 
+    def _send_dhcp_packet(self, datapath, dhcp_pkt):
+
+        pkt = packet.Packet()
+        pkt.add_protocol(ethernet.ethernet(src=self.hw_addr, dhcp_pkt.chadddr))
+        pkt.add_protocol(ipv4.ipv4(src=self.ip_addr, dst="255.255.255.255", proto=17))
+        pkt.add_protocol(udp.udp(src_port=67, dst_port=68))
+        pkt.add_protocol(dhcp_pkt)
+
+        self._send_packet(self, datapath, pkt)
+
     def _send_packet(self, datapath, port, pkt):
+
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         pkt.serialize()
